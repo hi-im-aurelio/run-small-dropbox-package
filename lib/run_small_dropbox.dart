fb8c2e6bbeee1b897fb66543f2b187cea2d01853
Farioso Fernando <farioso.f@gmail.com>
2024-01-01 02:31:56 +0200

    docstring

--- lib/run_small_dropbox.dart
+++ lib/run_small_dropbox.dart
@@ -1,5 +1,4 @@
 library run_small_dropbox;
 
-export 'src/dropbox_app.dart' hide DropboxApp;
 export 'src/dropbox_file.dart';
 export 'src/dropbox.dart';
--- lib/src/dropbox_app.dart
+++ lib/src/dropbox_app.dart
@@ -1,3 +1,7 @@
+/// Represents an instance of the Dropbox application.
+///
+/// The `DropboxApp` class encapsulates the access token required for
+/// authentication and authorization with the Dropbox API.
 class DropboxApp {
   final String _accessToken;
 
